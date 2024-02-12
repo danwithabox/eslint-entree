f81fc3cb74b6d32026324af3a562b5f16c77361b
danwithabox <danwithabox@gmail.com>
2024-02-12 22:43:17 +0100

    building with tsup and validating package with publint

---
+++ src/utils.eslint.ts
@@ -0,0 +1,43 @@
+import type { Rules } from "eslint-define-config";
+/**
+ * A simple helper to make working with rules in `@ts-check`-enabled files easier
+ * by returning a narrowly typed object with explicitly known keys. Useful when filtering.
+ */
+export function flatConfigDefineRules<T extends Partial<Rules>>(rules: T) { return rules; }
+
+/**
+ * Provide a rules object to receive a getter function that returns the option of the given rule.
+ * 
+ * Useful for sharing options between a base rule, and a rule that extends the base rule,
+ * e.g. in the case of `eslint-plugin-vue` extension rules and the corresponding base `@stylistic` rules.
+ */
+export function flatConfigAdoptOptionsFromRules<T extends Partial<Rules>>(rules: T) {
+    function getOptionsOf<K extends keyof T>(keyOfRule: K) { return rules[keyOfRule]; }
+    return { getOptionsOf, };
+}
+
+/**
+ * Filter rules for debugging purposes by either picking or excluding rules.
+ */
+export function flatConfigFilterRules<T extends Partial<Rules>>(
+    rules: T,
+    mode: "pick" | "exclude",
+    selection: Array<keyof T>,
+    debug: boolean = false
+): Partial<T> {
+    if ((mode === "exclude") && (selection.length === 0)) return rules;
+
+    const entries = Object.entries(rules);
+    const _rulesFiltered: Record<string, any> = {};
+    for (const [key, rule] of entries) {
+        if (rule !== void 0) {
+            const isEntryRuleInSelection = selection.includes(key);
+            if ((mode === "pick") && isEntryRuleInSelection) _rulesFiltered[key] = rule;
+            if ((mode === "exclude") && !isEntryRuleInSelection) _rulesFiltered[key] = rule;
+        }
+    }
+    const rulesFiltered = _rulesFiltered as Partial<T>;
+    const entriesFiltered = Object.entries(rulesFiltered);
+    if (debug) console.info(`Filtered ${entries.length} rules in "${mode}" mode down to ${entriesFiltered.length}:`, rulesFiltered);
+    return rulesFiltered;
+}
